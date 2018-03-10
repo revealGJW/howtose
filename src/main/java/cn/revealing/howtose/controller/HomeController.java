ca7551a9577f75928f8d58aa29648de59d5f95a9
GJW <490861454@qq.com>
2018-03-11 00:18:30 +0800

    update next

--- src/main/java/cn/revealing/howtose/controller/HomeController.java
+++ src/main/java/cn/revealing/howtose/controller/HomeController.java
@@ -5,6 +5,9 @@ import cn.revealing.howtose.services.CommentService;
 import cn.revealing.howtose.services.FollowService;
 import cn.revealing.howtose.services.QuestionService;
 import cn.revealing.howtose.services.UserService;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.ibatis.annotations.Param;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,10 +16,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
+import java.awt.print.Pageable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by GJW on 2017/5/17.
@@ -59,10 +66,19 @@ public class HomeController {
 
     @RequestMapping({"/", "/index"})
     public String index(Model model, HttpSession httpSession) {
-
-
-                model.addAttribute("vos", getQuestions(0, 0, 10));
+        model.addAttribute("vos", getQuestions(0, 0, 10));
         return "index";
+    }
+
+    @RequestMapping({"/getquestionByPage"})
+    @ResponseBody
+    public String index(@RequestParam("page") String page, HttpSession httpSession) {
+        Map<String, Object> map = new HashMap<>();
+        int pageNum = Integer.parseInt(page);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("rtnCode", 200);
+        jsonObject.put("bizData", getQuestions(0, (pageNum > 0 ? pageNum - 1 : 0) * 10, 10));
+        return jsonObject.toJSONString();
     }
 
     private List<ViewObject> getQuestions(int userId, int offset, int limit){
--- src/main/java/cn/revealing/howtose/model/ViewObject.java
+++ src/main/java/cn/revealing/howtose/model/ViewObject.java
@@ -7,11 +7,14 @@ import java.util.Map;
  * Created by GJW on 2017/5/17.
  */
 public class ViewObject {
-    private Map<String,Object> objs = new HashMap<String, Object>();
+    private Map<String, Object> objs = new HashMap<String, Object>();
     public void set(String key, Object value){
         objs.put(key, value);
     }
     public Object get(String key){
         return objs.get(key);
+    }
+    public Map<String, Object> getObjs() {
+        return objs;
     }
 }
