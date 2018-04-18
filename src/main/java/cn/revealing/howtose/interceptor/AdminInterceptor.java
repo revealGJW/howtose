7c36d9a7c22f2ef48394adce709c9ca680f66320
guojiawei02 <guojiawei02@meituan.com>
2018-04-18 13:01:15 +0800

    socre

---
+++ src/main/java/cn/revealing/howtose/interceptor/AdminInterceptor.java
@@ -0,0 +1,34 @@
+package cn.revealing.howtose.interceptor;
+
+import cn.revealing.howtose.model.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author guojiawei
+ * @date 2018/4/18
+ */
+public class AdminInterceptor implements HandlerInterceptor {
+    @Autowired
+    HostHolder hostHolder;
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        if(hostHolder.getUser() == null || hostHolder.getUser().)
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
